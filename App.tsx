d21f6b5a323af8dcee3a54f130c6e1a3b667ef45
Lucho Andrade <luan95_12@hotmail.com>
2020-04-01 12:06:09 -0500

    Initial release

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import appJson from "./app.json";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>React native boilerplate</Text>
+      <Text>v{appJson.expo.version}</Text>
     </View>
   );
 }
